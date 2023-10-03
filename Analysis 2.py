7e7df7e730d10344a240a3453c9994c65ac2e95b
Ibi Keller <ibi.keller@gmail.com>
2023-10-02 21:22:53 -0400

    Analysis 2: Added SEM, Formatted CI95%; Part 4 - Part 4: Implemented ANOVA for dep1lev3_g?_smok2

--- Analysis 2.py
+++ Analysis 2.py
@@ -4,9 +4,10 @@ import numpy as np
 import seaborn as sns
 
 from scipy import stats
+from scipy.stats import sem
 from scipy.stats import shapiro
 from scipy.stats import levene
-
+from scipy.stats import f_oneway
 
 # import main data file
 df = pd.read_excel(r'C:\Users\Ibi\Google Drive\School, UMSON\2023.FA, NRSG 795 (Biostatistics for Evidence Based Practice)\Analysis 2\NRSG795 Fall 23 Analysis 2.xlsx',
@@ -26,7 +27,8 @@ g2 = df[(df.group == 2)]
 
 g1_age = g1.loc[:, "age"]
 print("G1 Age: \t\tn =", len(g1_age), "\t\tMean: %.1f" % g1_age.mean(), "\tMedian: %.1f" % g1_age.median(),
-      "\tSt Dev: %.1f" % g1_age.std(), "\tRange: ", g1_age.min(), "-", g1_age.max())
+      "\tSt Dev: %.1f" % g1_age.std(), "\tRange: ", g1_age.min(), "-", g1_age.max(),
+      "\tSEM: %.2f" % sem(g1_age))
 
 g1_male = g1.loc[(df.sex == 1)].loc[:, "sex"]
 print("G1 Male: \t\tn =", len(g1_male), "\t\t%%: %.1f" % (len(g1_male) / len(g1) * 100))
@@ -35,21 +37,25 @@ print("G1 Female: \t\tn =", len(g1_female), "\t\t%%: %.1f" % (len(g1_female) / l
 
 g1_dep1 = g1.loc[:, "dep1"]
 print("G1 Dep1: \t\tn =", len(g1_dep1), "\t\tMean: %.1f" % g1_dep1.mean(), "\tMedian: %.1f" % g1_dep1.median(),
-      "\tSt Dev: %.1f" % g1_dep1.std(), "\tRange: ", g1_dep1.min(), "-", g1_dep1.max())
+      "\tSt Dev: %.1f" % g1_dep1.std(), "\tRange: ", g1_dep1.min(), "-", g1_dep1.max(),
+      "\tSEM: %.2f" % sem(g1_dep1))
 
 g1_anx1 = g1.loc[:, "anx1"]
 print("G1 Anx1: \t\tn =", len(g1_anx1), "\t\tMean: %.1f" % g1_anx1.mean(), "\tMedian: %.1f" % g1_anx1.median(),
-      "\tSt Dev: %.1f" % g1_anx1.std(), "\tRange: ", g1_anx1.min(), "-", g1_anx1.max())
+      "\tSt Dev: %.1f" % g1_anx1.std(), "\tRange: ", g1_anx1.min(), "-", g1_anx1.max(),
+      "\tSEM: %.2f" % sem(g1_anx1))
 
 g1_smok1 = g1.loc[:, "smok1"]
 print("G1 Smok1: \t\tn =", len(g1_smok1), "\t\tMean: %.1f" % g1_smok1.mean(), "\tMedian: %.1f" % g1_smok1.median(),
-      "\tSt Dev: %.1f" % g1_smok1.std(), "\tRange: ", g1_smok1.min(), "-", g1_smok1.max())
+      "\tSt Dev: %.1f" % g1_smok1.std(), "\tRange: ", g1_smok1.min(), "-", g1_smok1.max(),
+      "\tSEM: %.2f" % sem(g1_smok1))
 
 print("")
 
 g2_age = g2.loc[:, "age"]
 print("G2 Age: \t\tn =", len(g2_age), "\t\tMean: %.1f" % g2_age.mean(), "\tMedian: %.1f" % g2_age.median(),
-      "\tSt Dev: %.1f" % g2_age.std(), "\tRange: ", g2_age.min(), "-", g2_age.max())
+      "\tSt Dev: %.1f" % g2_age.std(), "\tRange: ", g2_age.min(), "-", g2_age.max(),
+      "\tSEM: %.2f" % sem(g2_age))
 
 g2_male = g2.loc[(df.sex == 1)].loc[:, "sex"]
 print("G2 Male: \t\tn =", len(g2_male), "\t\t%%: %.1f" % (len(g2_male) / len(g2) * 100))
@@ -58,15 +64,18 @@ print("G2 Female: \t\tn =", len(g2_female), "\t\t%%: %.1f" % (len(g2_female) / l
 
 g2_dep1 = g2.loc[:, "dep1"]
 print("G2 Dep1: \t\tn =", len(g2_dep1), "\t\tMean: %.1f" % g2_dep1.mean(), "\tMedian: %.1f" % g2_dep1.median(),
-      "\tSt Dev: %.1f" % g2_dep1.std(), "\tRange: ", g2_dep1.min(), "-", g2_dep1.max())
+      "\tSt Dev: %.1f" % g2_dep1.std(), "\tRange: ", g2_dep1.min(), "-", g2_dep1.max(),
+      "\tSEM: %.2f" % sem(g2_dep1))
 
 g2_anx1 = g2.loc[:, "anx1"]
 print("G2 Anx1: \t\tn =", len(g2_anx1), "\t\tMean: %.1f" % g2_anx1.mean(), "\tMedian: %.1f" % g2_anx1.median(),
-      "\tSt Dev: %.1f" % g2_anx1.std(), "\tRange: ", g2_anx1.min(), "-", g2_anx1.max())
+      "\tSt Dev: %.1f" % g2_anx1.std(), "\tRange: ", g2_anx1.min(), "-", g2_anx1.max(),
+      "\tSEM: %.2f" % sem(g2_anx1))
 
 g2_smok1 = g2.loc[:, "smok1"]
 print("G2 Smok1: \t\tn =", len(g2_smok1), "\t\tMean: %.1f" % g2_smok1.mean(), "\tMedian: %.1f" % g2_smok1.median(),
-      "\tSt Dev: %.1f" % g2_smok1.std(), "\tRange: ", g2_smok1.min(), "-", g2_smok1.max())
+      "\tSt Dev: %.1f" % g2_smok1.std(), "\tRange: ", g2_smok1.min(), "-", g2_smok1.max(),
+      "\tSEM: %.2f" % sem(g2_smok1))
 
 # ------------------------------------------------------------
 # PART 2: COMPARING TWO INDEPENDENT GROUP MEANS
@@ -105,18 +114,22 @@ print(" └→ \t", ind_ttest_smok1.confidence_interval(confidence_level=0.95))
 print("\n\nPART 3: COMPARING MEANS OF A SINGLE GROUP, PRE-TEST POST-TEST:\n")
 
 print("G1 Smok1: \t\tn =", len(g1_smok1), "\t\tMean: %.1f" % g1_smok1.mean(), "\tMedian: %.1f" % g1_smok1.median(),
-      "\tSt Dev: %.1f" % g1_smok1.std(), "\tRange: ", g1_smok1.min(), "-", g1_smok1.max())
+      "\tSt Dev: %.1f" % g1_smok1.std(), "\tRange: ", g1_smok1.min(), "-", g1_smok1.max(),
+      "\tSEM: %.2f" % sem(g1_smok1))
 g1_smok2 = g1.loc[:, "smok2"]
 print("G1 Smok2: \t\tn =", len(g1_smok2), "\t\tMean: %.1f" % g1_smok2.mean(), "\tMedian: %.1f" % g1_smok2.median(),
-      "\tSt Dev: %.1f" % g1_smok2.std(), "\tRange: ", g1_smok2.min(), "-", g1_smok2.max())
+      "\tSt Dev: %.1f" % g1_smok2.std(), "\tRange: ", g1_smok2.min(), "-", g1_smok2.max(),
+      "\t\tSEM: %.2f" % sem(g1_smok2))
 
 print("")
 
 print("G2 Smok1: \t\tn =", len(g2_smok1), "\t\tMean: %.1f" % g2_smok1.mean(), "\tMedian: %.1f" % g2_smok1.median(),
-      "\tSt Dev: %.1f" % g2_smok1.std(), "\tRange: ", g2_smok1.min(), "-", g2_smok1.max())
+      "\tSt Dev: %.1f" % g2_smok1.std(), "\tRange: ", g2_smok1.min(), "-", g2_smok1.max(),
+      "\tSEM: %.2f" % sem(g2_smok1))
 g2_smok2 = g2.loc[:, "smok2"]
 print("G2 Smok2: \t\tn =", len(g2_smok2), "\t\tMean: %.1f" % g2_smok2.mean(), "\tMedian: %.1f" % g2_smok2.median(),
-      "\tSt Dev: %.1f" % g2_smok2.std(), "\tRange: ", g2_smok2.min(), "-", g2_smok2.max())
+      "\tSt Dev: %.1f" % g2_smok2.std(), "\tRange: ", g2_smok2.min(), "-", g2_smok2.max(),
+      "\t\tSEM: %.2f" % sem(g2_smok2))
 
 print("")
 
@@ -137,9 +150,24 @@ print("")
 
 p1ttest_g1_smok = stats.ttest_rel(g1_smok1, g1_smok2, alternative="greater")
 print("G1_Smok? Paired 1-sided t Test: \tt = %.2f" % p1ttest_g1_smok.statistic, "\t\tp = %.4f" % p1ttest_g1_smok.pvalue)
-print(" └→ \t", p1ttest_g1_smok.confidence_interval(confidence_level=0.95))
+p1ttest_g1_smok_ci95 = p1ttest_g1_smok.confidence_interval(confidence_level=0.95)
+print(" └→ CI 95:\t%.2f" % p1ttest_g1_smok_ci95.low, "- %.2f" % p1ttest_g1_smok_ci95.high)
 
 p1ttest_g2_smok = stats.ttest_rel(g2_smok1, g2_smok2, alternative="greater")
 print("G2_Smok? Paired 1-sided t Test: \tt = %.2f" % p1ttest_g2_smok.statistic, "\t\tp = %.4f" % p1ttest_g2_smok.pvalue)
-print(" └→ \t", p1ttest_g2_smok.confidence_interval(confidence_level=0.95))
+p1ttest_g2_smok_ci95 = p1ttest_g2_smok.confidence_interval(confidence_level=0.95)
+print(" └→ CI 95:\t%.2f" % p1ttest_g2_smok_ci95.low, "- %.2f" % p1ttest_g2_smok_ci95.high)
+
+# ------------------------------------------------------------
+# PART 4: COMPARING MEANS OF MORE THAN 2 GROUPS
+# ------------------------------------------------------------
+
+print("\n\nPART 4: COMPARING MEANS OF MORE THAN 2 GROUPS:\n")
+
+dep1lev3_g1_smok2 = df[(df.dep1_lev3 == 1)].loc[:, "smok2"]
+dep1lev3_g2_smok2 = df[(df.dep1_lev3 == 2)].loc[:, "smok2"]
+dep1lev3_g3_smok2 = df[(df.dep1_lev3 == 3)].loc[:, "smok2"]
+
+anova_dep1lev3_smok2 = f_oneway(dep1lev3_g1_smok2, dep1lev3_g2_smok2, dep1lev3_g3_smok2)
+print("dep1lev3 G? One-Way ANOVA: \t\tF = %.2f" % anova_dep1lev3_smok2.statistic, "\t\tp = %.4f" % anova_dep1lev3_smok2.pvalue)
 
